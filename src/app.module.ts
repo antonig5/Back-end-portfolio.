1c69a061cd5f09b6af979c70de92984e2288384a
Yesid Hernandez <fyesid.h@gmail.com>
2023-07-05 16:41:20 -0500

    Cloudinary added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
 import { Module } from '@nestjs/common';
 
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { DataSource } from 'typeorm';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { CloudinaryProvider } from './cloudinary/cloudinary.provider';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({rootPath:join(__dirname,'..','public/dist')})
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public/dist'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    CloudinaryModule,
   ],
-  
+  providers: [CloudinaryProvider],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly dataSource: DataSource) {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+(async function () {
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+  await app.listen(PORT, () => {
+    console.log(`Portfolio running in the port: ${PORT}`);
+  });
+})();
